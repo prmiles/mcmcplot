d560802131cc3fd074e14051efa2b0ca458ecffe
prmiles <prmiles@ncsu.edu>
2018-08-09 16:13:49 -0400

    added long description

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ def read(fname):
     with codecs.open(fname, 'r', 'latin') as f:
         return f.read()
     
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with codecs.open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 # read in version number
 version_dummy = {}
 exec(read('mcmcplot/__version__.py'), version_dummy)
@@ -15,6 +21,7 @@ setup(
     name='mcmcplot',
     version=__version__,
     description='A library to plot and analyze chains from mcmc simulations',
+    long_description=long_description,
     url='https://github.com/prmiles/mcmcplot',
     download_url='https://github.com/prmiles/mcmcplot',
     author='Paul Miles',
