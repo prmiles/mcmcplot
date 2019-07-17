a378f5c0286b79894059ba31a119672e3cb1ce4d
prmiles <prmiles@ncsu.edu>
2019-07-17 11:11:51 -0400

    added imports to init

--- mcmcplot/__init__.py
+++ mcmcplot/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "0.1.0rc1"
+__version__ = "1.0.0rc1"
+
+from . import mcmatplot as _mcmpl
+from . import mcseaborn as _mcsns
+
+plot_density_panel = _mcmpl.plot_density_panel
+plot_histogram_panel = _mcmpl.plot_histogram_panel
+plot_chain_panel = _mcmpl.plot_chain_panel
+plot_pairwise_correlation_panel = _mcmpl.plot_pairwise_correlation_panel
+
+plot_joint_distributions = _mcsns.plot_joint_distributions
+plot_paired_density_matrix = _mcsns.plot_paired_density_matrix
